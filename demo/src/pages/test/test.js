4c6fc2d1e3d9f5caa25b26145f5fa1a507c7347e
从小就腹黑 <1059672188@qq.com>
2018-05-28 14:43:38 +0800

    1st
    1st

---
+++ demo/src/pages/test/test.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App'
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+})
